cacc35366e33c082a7967e770f88ede3a87cf3d3
showtimao10-dot <showtimao10@gmail.com>
2025-12-25 02:07:26 -0300

    Populate MODELOS with image data
    Added a list of model objects with IDs and image sources.

--- modelos/modelos.js
+++ modelos/modelos.js
@@ -1,7 +1,602 @@
-// modelos/modelos.js
-// Aqui você vai colar o JSON gerado pelo EXTRATOR_120.txt.
-
 window.MODELOS = [
-  // Exemplo:
-  // { id: "001", src: "https://site.com/imagens/001.jpg", tags: [] },
+  {
+    "id": "001",
+    "src": "https://img.cdndsgni.com/preview/10650052-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "002",
+    "src": "https://img.cdndsgni.com/preview/10654520-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "003",
+    "src": "https://img.cdndsgni.com/preview/12606640-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "004",
+    "src": "https://img.cdndsgni.com/preview/11806644-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "005",
+    "src": "https://img.cdndsgni.com/preview/12606634-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "006",
+    "src": "https://img.cdndsgni.com/preview/10650057-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "007",
+    "src": "https://img.cdndsgni.com/preview/12606566-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "008",
+    "src": "https://img.cdndsgni.com/preview/10669453-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "009",
+    "src": "https://img.cdndsgni.com/preview/10650062-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "010",
+    "src": "https://img.cdndsgni.com/preview/10087014-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "011",
+    "src": "https://img.cdndsgni.com/preview/12606632-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "012",
+    "src": "https://img.cdndsgni.com/preview/10999098-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "013",
+    "src": "https://img.cdndsgni.com/preview/11624115-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "014",
+    "src": "https://img.cdndsgni.com/preview/10128057-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "015",
+    "src": "https://img.cdndsgni.com/preview/13024947-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "016",
+    "src": "https://img.cdndsgni.com/preview/11806655-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "017",
+    "src": "https://img.cdndsgni.com/preview/11806645-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "018",
+    "src": "https://img.cdndsgni.com/preview/11806641-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "019",
+    "src": "https://img.cdndsgni.com/preview/11117441-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "020",
+    "src": "https://img.cdndsgni.com/preview/10689541-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "021",
+    "src": "https://img.cdndsgni.com/preview/11280157-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "022",
+    "src": "https://img.cdndsgni.com/preview/10650048-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "023",
+    "src": "https://img.cdndsgni.com/preview/10650053-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "024",
+    "src": "https://img.cdndsgni.com/preview/10128056-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "025",
+    "src": "https://img.cdndsgni.com/preview/11362523-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "026",
+    "src": "https://img.cdndsgni.com/preview/10025558-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "027",
+    "src": "https://img.cdndsgni.com/preview/12606611-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "028",
+    "src": "https://img.cdndsgni.com/preview/12606575-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "029",
+    "src": "https://img.cdndsgni.com/preview/12606643-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "030",
+    "src": "https://img.cdndsgni.com/preview/12606637-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "031",
+    "src": "https://img.cdndsgni.com/preview/12606633-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "032",
+    "src": "https://img.cdndsgni.com/preview/12606630-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "033",
+    "src": "https://img.cdndsgni.com/preview/12606616-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "034",
+    "src": "https://img.cdndsgni.com/preview/12606577-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "035",
+    "src": "https://img.cdndsgni.com/preview/12606552-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "036",
+    "src": "https://img.cdndsgni.com/preview/12606510-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "037",
+    "src": "https://img.cdndsgni.com/preview/11117443-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "038",
+    "src": "https://img.cdndsgni.com/preview/11117430-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "039",
+    "src": "https://img.cdndsgni.com/preview/11117427-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "040",
+    "src": "https://img.cdndsgni.com/preview/11117412-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "041",
+    "src": "https://img.cdndsgni.com/preview/11117352-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "042",
+    "src": "https://img.cdndsgni.com/preview/10998578-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "043",
+    "src": "https://img.cdndsgni.com/preview/10996675-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "044",
+    "src": "https://img.cdndsgni.com/preview/11886695-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "045",
+    "src": "https://img.cdndsgni.com/preview/12977139-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "046",
+    "src": "https://img.cdndsgni.com/preview/12517378-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "047",
+    "src": "https://img.cdndsgni.com/preview/10129168-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "048",
+    "src": "https://img.cdndsgni.com/preview/13469680-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "049",
+    "src": "https://img.cdndsgni.com/preview/10650058-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "050",
+    "src": "https://img.cdndsgni.com/preview/10178002-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "051",
+    "src": "https://img.cdndsgni.com/preview/12606639-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "052",
+    "src": "https://img.cdndsgni.com/preview/12606638-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "053",
+    "src": "https://img.cdndsgni.com/preview/12606635-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "054",
+    "src": "https://img.cdndsgni.com/preview/12606629-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "055",
+    "src": "https://img.cdndsgni.com/preview/12606627-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "056",
+    "src": "https://img.cdndsgni.com/preview/12606625-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "057",
+    "src": "https://img.cdndsgni.com/preview/12606624-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "058",
+    "src": "https://img.cdndsgni.com/preview/12606623-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "059",
+    "src": "https://img.cdndsgni.com/preview/12606622-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "060",
+    "src": "https://img.cdndsgni.com/preview/12606620-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "061",
+    "src": "https://img.cdndsgni.com/preview/12606619-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "062",
+    "src": "https://img.cdndsgni.com/preview/12606618-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "063",
+    "src": "https://img.cdndsgni.com/preview/12606617-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "064",
+    "src": "https://img.cdndsgni.com/preview/12606614-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "065",
+    "src": "https://img.cdndsgni.com/preview/12606613-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "066",
+    "src": "https://img.cdndsgni.com/preview/12606612-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "067",
+    "src": "https://img.cdndsgni.com/preview/12606610-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "068",
+    "src": "https://img.cdndsgni.com/preview/12606608-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "069",
+    "src": "https://img.cdndsgni.com/preview/12606607-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "070",
+    "src": "https://img.cdndsgni.com/preview/12606606-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "071",
+    "src": "https://img.cdndsgni.com/preview/12606605-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "072",
+    "src": "https://img.cdndsgni.com/preview/12606604-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "073",
+    "src": "https://img.cdndsgni.com/preview/12606603-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "074",
+    "src": "https://img.cdndsgni.com/preview/12606602-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "075",
+    "src": "https://img.cdndsgni.com/preview/12606600-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "076",
+    "src": "https://img.cdndsgni.com/preview/12606598-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "077",
+    "src": "https://img.cdndsgni.com/preview/12606597-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "078",
+    "src": "https://img.cdndsgni.com/preview/12606596-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "079",
+    "src": "https://img.cdndsgni.com/preview/12606595-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "080",
+    "src": "https://img.cdndsgni.com/preview/12606594-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "081",
+    "src": "https://img.cdndsgni.com/preview/12606593-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "082",
+    "src": "https://img.cdndsgni.com/preview/12606592-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "083",
+    "src": "https://img.cdndsgni.com/preview/12606590-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "084",
+    "src": "https://img.cdndsgni.com/preview/12606587-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "085",
+    "src": "https://img.cdndsgni.com/preview/12606585-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "086",
+    "src": "https://img.cdndsgni.com/preview/12606584-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "087",
+    "src": "https://img.cdndsgni.com/preview/12606583-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "088",
+    "src": "https://img.cdndsgni.com/preview/12606579-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "089",
+    "src": "https://img.cdndsgni.com/preview/12606578-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "090",
+    "src": "https://img.cdndsgni.com/preview/12606572-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "091",
+    "src": "https://img.cdndsgni.com/preview/12606571-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "092",
+    "src": "https://img.cdndsgni.com/preview/12606570-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "093",
+    "src": "https://img.cdndsgni.com/preview/12606569-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "094",
+    "src": "https://img.cdndsgni.com/preview/12606564-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "095",
+    "src": "https://img.cdndsgni.com/preview/12606562-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "096",
+    "src": "https://img.cdndsgni.com/preview/12606560-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "097",
+    "src": "https://img.cdndsgni.com/preview/12606558-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "098",
+    "src": "https://img.cdndsgni.com/preview/12606557-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "099",
+    "src": "https://img.cdndsgni.com/preview/12606549-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "100",
+    "src": "https://img.cdndsgni.com/preview/12606548-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "101",
+    "src": "https://img.cdndsgni.com/preview/12606546-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "102",
+    "src": "https://img.cdndsgni.com/preview/12606545-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "103",
+    "src": "https://img.cdndsgni.com/preview/12606544-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "104",
+    "src": "https://img.cdndsgni.com/preview/12606541-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "105",
+    "src": "https://img.cdndsgni.com/preview/12606540-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "106",
+    "src": "https://img.cdndsgni.com/preview/12606539-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "107",
+    "src": "https://img.cdndsgni.com/preview/12606536-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "108",
+    "src": "https://img.cdndsgni.com/preview/12606535-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "109",
+    "src": "https://img.cdndsgni.com/preview/12606534-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "110",
+    "src": "https://img.cdndsgni.com/preview/12606532-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "111",
+    "src": "https://img.cdndsgni.com/preview/12606531-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "112",
+    "src": "https://img.cdndsgni.com/preview/12606530-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "113",
+    "src": "https://img.cdndsgni.com/preview/12606527-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "114",
+    "src": "https://img.cdndsgni.com/preview/12606526-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "115",
+    "src": "https://img.cdndsgni.com/preview/12606525-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "116",
+    "src": "https://img.cdndsgni.com/preview/12606524-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "117",
+    "src": "https://img.cdndsgni.com/preview/12606523-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "118",
+    "src": "https://img.cdndsgni.com/preview/12606522-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "119",
+    "src": "https://img.cdndsgni.com/preview/12606521-m.jpg",
+    "tags": []
+  },
+  {
+    "id": "120",
+    "src": "https://img.cdndsgni.com/preview/12606518-m.jpg",
+    "tags": []
+  }
 ];
